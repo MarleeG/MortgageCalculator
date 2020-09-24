6e9469148e9e52858ff77b04e8f805f0861a66cb
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-09-24 19:13:10 -0400

    homepage initial setup complete

---
+++ client/src/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import "./Home.css";
+const Base = () => {
+    return <div className="home__container center">
+        <h1>Mortage Calculator</h1>
+        <div className="container__home-fields">
+            
+        </div>
+    </div>
+}
+
+export default Base;
