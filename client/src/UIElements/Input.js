db220188ab254405fe4164c7ce57cc863fa76a62
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-09-25 22:24:58 -0400

    onChange is now been added to first input

--- client/src/UIElements/Input.js
+++ client/src/UIElements/Input.js
@@ -2,23 +2,29 @@ import React, { Fragment } from "react";
 
 import "./Input.css";
 
+const log = console.log;
 const Input = (props) => {
+  log(props);
 
-  const inputStyles ={
+  const inputStyles = {
     width: "17vw",
     height: "2.5vw",
     border: "1px solid black",
     margin: "10px",
-    borderRadius: "3px"
-
-
+    borderRadius: "3px",
   };
-
 
   return (
     <Fragment>
-        <label>{props.for}</label>
-      <input style={inputStyles} value={props.value || ""} placeholder={props.placeholder || ""} />
+      <label>{props.for}</label>
+      <input
+        value={props.value}
+        name={props.name}
+        type={props.type}
+        placeholder={props.placeholder || "placeholder"}
+        onChange={(e) => props.handleChange(e)}
+        style={inputStyles}
+      />
     </Fragment>
   );
 };
